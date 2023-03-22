2417a3c47a9c54d41e5fb3a87388903f920ac82c
Enzo Bakri <bakrienzo@gmail.com>
2023-03-22 14:35:38 +0100

    Initial commit

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,23 +1,7 @@
-import logo from '../../assets'
 import '../../styles/Home.css'
 
 function Home() {
-  return (
-    <div className="Home">
-      <header className="Home-header">
-        <img src={logo} className="Home-logo" alt="logo" />
-        <h1>Hello World 👋</h1>
-        <a
-          className="Home-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+  return <div>Page d'accueil 🏡</div>
 }
 
 export default Home
